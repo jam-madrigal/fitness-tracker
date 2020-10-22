49e3c0ff888f9acddaf5aadf25b874d1f1fee357
Alex Madrigal <jam.madrigal@gmail.com>
2020-10-21 22:41:06 -0700

    deploying

---
+++ server.js
@@ -0,0 +1,14 @@
+
+
+
+
+// Connect to the mongodb database
+   mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
