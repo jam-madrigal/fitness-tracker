d62cd2fa94ecd7a97ac07da0bcc0aa7d6b9dda72
Alex Madrigal <jam.madrigal@gmail.com>
2020-10-24 06:23:35 -0700

    updated the apiroutes syntax to es6

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -19,7 +19,7 @@ const Workout = require('../models/workout.js')
       });
 
     // Get all the workouts from the db, unsorted, for the /stats page
-      app.get("/api/workouts/range", function(req, res) {
+      app.get("/api/workouts/range", (req, res) => {
         Workout.find({})
         .then(dbWorkouts => {
           res.json(dbWorkouts);
@@ -33,14 +33,14 @@ const Workout = require('../models/workout.js')
       // Below code handles when a user submits a form and thus submits data to the server.
 
     // Create a new workout
-      app.post("/api/workouts", function({body}, res) {
+      app.post("/api/workouts", ({body}, res) => {
 // Add a Workout.create() method that creates a new workout, taking in the body of the request as keys/values
         console.log(body);
         res.json();
       });
 
     // API PUT requests to update add a new exercise to a workout in the db
-      app.put("/api/workouts/:id", function({body, params}, res) {
+      app.put("/api/workouts/:id", ({body, params}, res) => {
         // Grab the ID of the previous workout, then add code to add to that database entry based on the id
 
         Workout.findOneAndUpdate({_id: params.id}, { $push: { exercises: body } }, {runValidators: true})
